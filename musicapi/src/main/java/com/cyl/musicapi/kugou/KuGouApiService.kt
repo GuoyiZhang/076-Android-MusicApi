a8e5d8c0911d8051c9c499bfe5abbe8189f7bbef
guoyi <1806242240@qq.com>
2020-02-24 16:30:19 +0800

    up

--- musicapi/src/main/java/com/cyl/musicapi/kugou/KuGouApiService.kt
+++ musicapi/src/main/java/com/cyl/musicapi/kugou/KuGouApiService.kt
@@ -1,5 +1,7 @@
 package com.cyl.musicapi.kugou
 
+import com.cyl.musicapi.baidu.BaiduApiService
+import com.cyl.musicapi.baidu.BaiduSearchMergeInfo
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,5 +13,13 @@ interface KuGouApiService {
 
     @GET("download?ver=1&client=pc&fmt=lrc&charset=utf8")
     fun getRawLyric(@Query("id") id: String, @Query("accesskey") accesskey: String): Observable<KugouLyricInfo>
+
+    /**
+     * 搜索
+     */
+    @GET("api/v3/search/song?format=json")
+    fun queryMerge(@Query("keyword") keyword: String,
+                   @Query("page") page: Int,
+                   @Query("pagesize") pagesize: Int): Observable<BaiduSearchMergeInfo>
 
 }
